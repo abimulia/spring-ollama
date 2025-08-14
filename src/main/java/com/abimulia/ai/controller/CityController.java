fe6cb0c620ccbfb598b0510e84f3bd8d3714c62c
amuli4 <abimulia@outlook.com>
2025-08-14 08:51:18 +0700

    changes to adapt to spring ai ver 1.0.1

--- src/main/java/com/abimulia/ai/controller/CityController.java
+++ src/main/java/com/abimulia/ai/controller/CityController.java
@@ -4,6 +4,12 @@
  */
 package com.abimulia.ai.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.function.Function;
 /**
  * @author abimu
  *
@@ -13,6 +19,25 @@ package com.abimulia.ai.controller;
  * 
  * Copyright(c) 2025 Abi Mulia
  */
+
+@RestController
 public class CityController {
+	
+	private final ChatClient chatClient;
+	
+	public CityController(ChatClient.Builder builder,Function currentWeatherFunction ) {
+        this.chatClient = builder
+                .defaultSystem("You are a helpful AI Assistant answering questions about cities around the world.")
+                .defaultFunctions(currentWeatherFunction)
+                .build();
+    }
+	
+	@GetMapping("/cities")
+	public String cityFaq(@RequestParam String message) {
+		return chatClient.prompt()
+				.user(message)
+				.call()
+				.content();
+	}
 
 }
